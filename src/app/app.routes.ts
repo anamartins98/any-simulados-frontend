2c366e3fc7e8e0a5247d56a4c2d04f3480464a72
Ana Martins <anacarolina.bnl@gmail.com>
2025-04-07 22:28:12 -0300

    criando funcionalidades

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ConsultarComponent } from './feature/questoes/consultar/consultar.component';
+import { CadastrarComponent } from './feature/questoes/cadastrar/cadastrar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'questoes/consultar', component: ConsultarComponent },
+    { path: 'questoes/cadastrar', component: CadastrarComponent  }
+];
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatButtonModule, MatMenuModule, MatIconModule],
+  imports: [MatButtonModule, MatMenuModule, MatIconModule, RouterModule],
   standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
